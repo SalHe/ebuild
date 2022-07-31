e9d2527d3ecc13e17e07dbc978547978b825ad51
SalHe Li <SalHe@qq.com>
2022-08-01 07:42:51 +0800

    docs(vitepress): markdown-it-task-checkbox

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vitepress";
+import miCheckbox from "markdown-it-task-checkbox";
 
 export default defineConfig({
   base: '/ebuild/',
   title: 'ebuild',
-  description: 'ebuild 是一个专注于易语言的构建工具。'
+  description: 'ebuild 是一个专注于易语言的构建工具。',
+  markdown: {
+    config: (md) => {
+      md.use(miCheckbox, { readonly: true })
+    }
+  }
 })
